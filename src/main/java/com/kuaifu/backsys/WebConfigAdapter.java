8c5165d072032b8ae4d720343217ca659395e3ed
MSI <MSI@DESKTOP-6OVABIB>
2017-12-17 14:07:02 +0800

    Add Adapter

---
+++ src/main/java/com/kuaifu/backsys/WebConfigAdapter.java
@@ -0,0 +1,14 @@
+package com.kuaifu.backsys;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfigAdapter extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**/*").allowedOrigins("*").allowedMethods("POST","GET");
+	}
+}
